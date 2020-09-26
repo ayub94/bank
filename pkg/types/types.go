a42acfd9d16a0a4aa6db9661a1e182663f45c155
Ayub <ayubjon94.an@gmail.com>
2020-09-26 13:46:07 +0500

    добавлени Status в types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,21 @@ type Money int64
 // Category представляет с собой котегори, в которо был совершён платеж (авто, аптеки , ресторани и т д)
 type Category string
 
-// 
+// Status представляет с собой статус платежей
+type Status string
+
+// Предопределенные статусы платежей
+const (
+    StatusOk    Status = "Ok"
+    StatusFail    Status = "FAIL"
+    StatusInProgress   Status = "INPROGRESS"
+)
+
+
+// Payment
 type Payment struct{
 	ID                 int
     Amount             Money
     Category           Category
+    Status             Status
 }
